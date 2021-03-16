d46e5ddca79b45a4a29f2229f4ea35f52c35bbd7
Shannon Ervin <sjervin07@gmail.com>
2021-03-16 00:02:18 -0500

    pagination, trending finished

--- whattowatch/src/Pages/Trending/Trending.js
+++ whattowatch/src/Pages/Trending/Trending.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
+import CustomPagination from "../../Components/Pagination/CustomPagination";
 import SingleContent from "../../Components/SingleContent/SingleContent";
 import "./Trending.css"
 
 const Trending = () => {
-
+    const [page, setPage] = useState(1)
     const [content,setContent] = useState([])
 
     const fetchTrending =  async () => {
-        const {data} = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        const {data} = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
         );
 
         //console.log(data)
@@ -18,7 +19,7 @@ const Trending = () => {
 
     useEffect(() => { 
        fetchTrending();          
-    }, [])
+    }, [page])
 
     return (
         <div>
@@ -35,6 +36,7 @@ const Trending = () => {
                     />
                 )}    
             </div>
+            <CustomPagination setPage={setPage}/>
         </div>
     );
 };
